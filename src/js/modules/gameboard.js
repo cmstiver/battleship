fa842d639143d86a0bb2fb8560ebbdc622b0377c
Chayse Stiver <cmstiver1@gmail.com>
2021-07-27 00:03:53 -0700

    added y axis to placeShip()

--- src/js/modules/gameboard.js
+++ src/js/modules/gameboard.js
@@ -9,6 +9,11 @@ function createGameBoard() {
           const newCoords = [coords[0] + i, coords[1]];
           this.shipPositions[ship.name].coords.push(newCoords);
         }
+      } else if (axis === 'y') {
+        for (let i = 0; i < ship.length; i++) {
+          const newCoords = [coords[0], coords[1] + i];
+          this.shipPositions[ship.name].coords.push(newCoords);
+        }
       }
     },
     receiveAttack() {},
