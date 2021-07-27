919d41d915c090b6f78283d8eb8fc7e9543c65ec
Chayse Stiver <cmstiver1@gmail.com>
2021-07-27 15:40:54 -0700

    refactored createGameBoard

--- src/js/modules/gameboard.js
+++ src/js/modules/gameboard.js
@@ -1,53 +1,57 @@
 function createGameBoard() {
+  const boardState = {
+    ships: {},
+    dodgedShots: [],
+  };
+  const isGameOver = () => {
+    const { ships } = boardState;
+    const shipsArray = Object.keys(ships);
+    const sunkArray = [];
+    shipsArray.forEach((ship) => {
+      sunkArray.push(ships[ship].isSunk());
+    });
+    if (sunkArray.every((value) => value === true)) {
+      return true;
+    }
+    return false;
+  };
+  const placeShip = (ship, coords, axis) => {
+    let shipObj = boardState.ships[ship.name];
+    shipObj = { ...shipObj, ...ship, ...{ coords: {} } };
+    if (axis === 'x') {
+      for (let i = 0; i < ship.length; i++) {
+        const newCoords = [coords[0] + i, coords[1]];
+        shipObj.coords[newCoords] = { isHit: false };
+      }
+    } else if (axis === 'y') {
+      for (let i = 0; i < ship.length; i++) {
+        const newCoords = [coords[0], coords[1] + i];
+        shipObj.coords[newCoords] = { isHit: false };
+      }
+    }
+    boardState.ships[ship.name] = shipObj;
+  };
+  const receiveAttack = (coords) => {
+    const coordsStr = coords.toString();
+    const { ships } = boardState;
+    const shipsArray = Object.keys(ships);
+    let hit = false;
+    shipsArray.forEach((ship) => {
+      if (Object.keys(ships[ship].coords).includes(coordsStr) === true) {
+        ships[ship].hit(coords);
+        hit = true;
+      }
+    });
+    if (hit === false) {
+      boardState.dodgedShots.push(coords);
+    }
+    isGameOver();
+  };
   return {
-    placeShip(ship, coords, axis) {
-      let shipObj = this.boardState.ships[ship.name];
-      shipObj = { ...shipObj, ...ship, ...{ coords: {} } };
-      if (axis === 'x') {
-        for (let i = 0; i < ship.length; i++) {
-          const newCoords = [coords[0] + i, coords[1]];
-          shipObj.coords[newCoords] = { isHit: false };
-        }
-      } else if (axis === 'y') {
-        for (let i = 0; i < ship.length; i++) {
-          const newCoords = [coords[0], coords[1] + i];
-          shipObj.coords[newCoords] = { isHit: false };
-        }
-      }
-      this.boardState.ships[ship.name] = shipObj;
-    },
-    receiveAttack(coords) {
-      const coordsStr = coords.toString();
-      const { ships } = this.boardState;
-      const shipsArray = Object.keys(ships);
-      let hit = false;
-      shipsArray.forEach((ship) => {
-        if (Object.keys(ships[ship].coords).includes(coordsStr) === true) {
-          ships[ship].hit(coords);
-          hit = true;
-        }
-      });
-      if (hit === false) {
-        this.boardState.dodgedShots.push(coords);
-      }
-      this.isGameOver();
-    },
-    isGameOver() {
-      const { ships } = this.boardState;
-      const shipsArray = Object.keys(ships);
-      const sunkArray = [];
-      shipsArray.forEach((ship) => {
-        sunkArray.push(ships[ship].isSunk());
-      });
-      if (sunkArray.every((value) => value === true)) {
-        return true;
-      }
-      return false;
-    },
-    boardState: {
-      ships: {},
-      dodgedShots: [],
-    },
+    placeShip,
+    receiveAttack,
+    isGameOver,
+    boardState,
   };
 }
 
