3e02712ac19bd694dc4b6ddcd3d5865b670a9b32
Chayse Stiver <cmstiver1@gmail.com>
2021-07-29 03:42:31 -0700

    human players can now take turns

--- src/js/modules/shipPlacerDOM.js
+++ src/js/modules/shipPlacerDOM.js
@@ -8,8 +8,9 @@ const place = (() => {
   let axis = 'x';
   let num = 0;
   let shadowLength = 5;
-  const markShipPos = (player) => {
-    const { ships } = Boards[player].boardState;
+  let player = 'player1Board';
+  const markShipPos = (plr) => {
+    const { ships } = Boards[plr].boardState;
     const shipsArray = Object.keys(ships);
     shipsArray.forEach((ship) => {
       const shipCoords = Object.keys(ships[ship].coords);
@@ -25,40 +26,49 @@ const place = (() => {
     const newCoord = split.map((x) => +x);
     switch (num) {
       case 0:
-        if (Boards.player1Board.placeShip(carrier, newCoord, axis) === true) {
+        if (Boards[player].placeShip(carrier, newCoord, axis) === true) {
           num++;
           shadowLength = 4;
-          markShipPos('player1Board');
+          markShipPos(player);
         }
         break;
       case 1:
-        if (Boards.player1Board.placeShip(battleship, newCoord, axis) === true) {
+        if (Boards[player].placeShip(battleship, newCoord, axis) === true) {
           num++;
           shadowLength = 3;
-          markShipPos('player1Board');
+          markShipPos(player);
         }
         break;
       case 2:
-        if (Boards.player1Board.placeShip(destroyer, newCoord, axis) === true) {
+        if (Boards[player].placeShip(destroyer, newCoord, axis) === true) {
           num++;
-          markShipPos('player1Board');
+          markShipPos(player);
         }
         break;
       case 3:
-        if (Boards.player1Board.placeShip(submarine, newCoord, axis) === true) {
+        if (Boards[player].placeShip(submarine, newCoord, axis) === true) {
           num++;
           shadowLength = 2;
-          markShipPos('player1Board');
+          markShipPos(player);
         }
         break;
       case 4:
-        if (Boards.player1Board.placeShip(patrolBoat, newCoord, axis) === true) {
+        if (Boards[player].placeShip(patrolBoat, newCoord, axis) === true) {
           document.querySelector('.start').remove();
-          DOM.addEventListeners();
-          DOM.setTurn('player1Board');
-          if (Players.player2.type === 'comp') {
-            DOM.markShipPos('player1Board');
+          num = 0;
+          shadowLength = 5;
+          axis = 'x';
+          if (player === 'player1Board') {
+            makeSecondWindow();
           }
+          if (player === 'player2Board') {
+            DOM.addEventListeners();
+            DOM.setTurn('player1Board');
+            if (Players.player2.type === 'comp') {
+              DOM.markShipPos(player);
+            }
+          }
+          player = 'player2Board';
         }
         break;
       default:
@@ -129,6 +139,24 @@ const place = (() => {
       square.addEventListener('mouseover', hoverShadow);
       square.addEventListener('mouseleave', removeShadow);
     });
+  };
+  const makeSecondWindow = () => {
+    document.querySelector('#start-container').innerHTML = `
+        <div class="start">
+          <div id="shipPlacer" class="gameboard"></div>
+          <button>X</button>
+        </div>
+    `;
+    const start = document.querySelector('.start #shipPlacer');
+    for (let i = 0; i < 100; i++) {
+      const y = Math.floor((i + 10) / 10);
+      const x = i - Math.floor(i / 10) * 10 + 1;
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.setAttribute('data-coord', `${x},${y}`);
+      start.appendChild(square);
+    }
+    addDOMStuff();
   };
 
   return {
